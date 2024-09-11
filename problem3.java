7b16fe819a42e242db60ca2f26b26019c08bdf2a
kdoann3 <kbdoan103@student.hbuhsd.edu>
2024-09-11 10:47:54 -0700

    ehehhe

--- problem3.java
+++ problem3.java
@@ -1,1 +1,11 @@
+public class problem3
+{
+  public static void main(String[] args)
+  {
+    System.out.println("=========");
+    System.out.println("= 0   0 =");
+    System.out.println("=   v   =");
+    System.out.print("=========");
+  }
+}
 
